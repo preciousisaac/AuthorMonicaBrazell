85fc1ad9ea298174926a5c798daee9506081ebf6
preciousisaac <isaacpreciouschampion@gmail.com>
2025-08-01 14:23:57 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// --------------scrooll behaviour-----------------
+
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      }
+    });
+  });
+
---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Toggle navbar
+const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Navbar scroll animation
+const navbar = document.getElementById('navbar');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+
+
+// Date
